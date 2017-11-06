496aca2fbb17f2eede7da298d0f5a4470718a173
Kevin Lai <kevinlai@Kevins-MacBook-Pro.local>
2017-11-06 13:11:34 +1100

    correct syntax errors

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,9 +1,9 @@
 class Accommodation < ApplicationRecord
   belongs_to :user
 
-  validates :land_type, presence :true
-  validates :accommodation_type, presence :true
+  validates :land_type, presence: true
+  validates :accommodation_type, presence: true
   
-  validates :area, presence :true
-  validates :capacity, presence :true
+  validates :area, presence: true
+  validates :capacity, presence: true
 end
